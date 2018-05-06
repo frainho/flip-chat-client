a10a73e9f5ed6efb6ca11e0b7040984d07241674
Filipe Rainho <filipenrainho@gmail.com>
2018-05-06 20:39:10 +0200

    update to angular v6

--- src/app/services/sockets.service.ts
+++ src/app/services/sockets.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
+import { Observable ,  Subject } from 'rxjs';
 import * as io from 'socket.io-client';
 import { environment } from '../../environments/environment';
 
